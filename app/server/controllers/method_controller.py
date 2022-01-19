2f02b6ff3c3d0b107123f815cb44d0b52696521e
Alejandro Miranda <alejandrom@secret-source.eu>
2022-01-19 14:29:32 +0000

    Función para buscar por id

--- app/server/controllers/method_controller.py
+++ app/server/controllers/method_controller.py
@@ -1,3 +1,5 @@
+from bson import ObjectId
+
 from app.server.database import methods_collection, methods_helper
 
 
@@ -6,3 +8,8 @@ async def find_all():
     for m in methods_collection.find():
         methods.append(methods_helper(m))
     return methods
+
+
+async def find_by_id(method_id):
+    method = await methods_collection.find_one({"_id": ObjectId(method_id)})
+    return method
