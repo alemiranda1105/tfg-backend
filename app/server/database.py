8fc27e202560069b10b74ef970b29e0578fa7df9
Alejandro Miranda <alejandrom@secret-source.eu>
2022-01-19 15:37:17 +0000

    Carga de base de dataos dependiendo del entorno

--- app/server/database.py
+++ app/server/database.py
@@ -1,3 +1,4 @@
+import sys
 import os
 
 import pymongo
@@ -5,9 +6,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-MONGO_DETAILS = os.getenv('DATABASE')
-client = pymongo.MongoClient(MONGO_DETAILS)
-
-db = client['tfg']
+if 'pytest' in sys.argv[0]:
+    MONGO_DETAILS = os.getenv('DATABASE_TEST')
+    client = pymongo.MongoClient(MONGO_DETAILS)
+    db = client['tfg_test']
+else:
+    MONGO_DETAILS = os.getenv('DATABASE')
+    client = pymongo.MongoClient(MONGO_DETAILS)
+    db = client['tfg']
 
 methods_collection = db["methods"]
