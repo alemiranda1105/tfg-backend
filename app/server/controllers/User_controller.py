74bb974e5a64a6c1d4c8b05382ed3b00aa5a9eb5
Alejandro Miranda <alejandrom@secret-source.eu>
2022-01-21 13:43:30 +0000

    JWT creado al registrar usuarios

--- app/server/controllers/User_controller.py
+++ app/server/controllers/User_controller.py
@@ -1,16 +1,15 @@
-import hashlib
+from pymongo.errors import DuplicateKeyError
 
 from app.server.database import users_collection
 from app.server.helpers.Helpers import users_helper
+from app.server.utils.Utils import hash_password
 
 
 async def create_user(user):
     user['password'] = hash_password(user['password'])
-    u = users_collection.insert_one(user)
-    new_user = users_collection.find_one({"_id": u.inserted_id})
-    return users_helper(new_user)
-
-
-def hash_password(password: str) -> str:
-    password = hashlib.md5(password.encode('utf-8'))
-    return password.hexdigest()
+    try:
+        u = users_collection.insert_one(user)
+        new_user = users_collection.find_one({"_id": u.inserted_id})
+        return users_helper(new_user)
+    except DuplicateKeyError:
+        return False
