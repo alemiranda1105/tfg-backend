2bc2e78c822e48c7c7f37237b371963244f6118f
Alejandro Miranda <alejandrom@secret-source.eu>
2022-01-21 11:29:33 +0000

    Hash de la contraseña

--- app/server/controllers/User_controller.py
+++ app/server/controllers/User_controller.py
@@ -1,8 +1,16 @@
+import hashlib
+
 from app.server.database import users_collection
 from app.server.helpers.Helpers import users_helper
 
 
 async def create_user(user):
+    user['password'] = hash_password(user['password'])
     u = users_collection.insert_one(user)
     new_user = users_collection.find_one({"_id": u.inserted_id})
     return users_helper(new_user)
+
+
+def hash_password(password: str) -> str:
+    password = hashlib.md5(password.encode('utf-8'))
+    return password.hexdigest()
