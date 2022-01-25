f522f38db20792ead8ef2affb8d99326a32cae10
Alejandro Miranda <alejandrom@secret-source.eu>
2022-01-25 17:20:53 +0000

    Actualizados datos de prueba

--- app/tests/mocked_test_data.py
+++ app/tests/mocked_test_data.py
@@ -8,48 +8,21 @@ methods_data_test = [
             "name": "test",
             "info": "This is an example",
             "link": "www.example.com",
-            "results": [
-                {
-                    "name": "m1",
-                    "result": 0.9192
-                },
-                {
-                    "name": "m2",
-                    "result": 0.5421
-                }
-            ]
+            "results": []
         },
         {
             "user_id": "2",
             "name": "test2",
             "info": "This is an example2",
             "link": "www.example.com",
-            "results": [
-                {
-                    "name": "m1",
-                    "result": 0.8123
-                },
-                {
-                    "name": "m2",
-                    "result": 0.7263
-                }
-            ]
+            "results": []
         },
         {
             "user_id": "3",
             "name": "test3",
             "info": "This is an example3",
             "link": "www.example.com",
-            "results": [
-                {
-                    "name": "m1",
-                    "result": 0.8123
-                },
-                {
-                    "name": "m2",
-                    "result": 0.7263
-                }
-            ]
+            "results": []
         }
     ]
 
