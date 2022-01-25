b69441160e1e0289cc0be02e6ee424fe7688ed81
Alejandro Miranda <alejandrom@secret-source.eu>
2022-01-25 17:17:28 +0000

    Comparación del modelo subido con el model base

--- app/server/evaluation/evaluation.py
+++ app/server/evaluation/evaluation.py
@@ -1,37 +1,64 @@
+import os
 import time
 import numpy as np
 from sklearn.metrics import recall_score, f1_score, precision_score
+from dotenv import load_dotenv
 
 from app.server.utils.Utils import extract_zip, get_files, json_to_dict
 
-
-def get_recall_score(to_compare):
-    return recall_score(to_compare, to_compare, average='micro')
+load_dotenv()
 
 
-def get_f1_score(to_compare):
-    return f1_score(to_compare, to_compare, average='micro', zero_division=1)
+def get_base_model():
+    folder_name = os.getenv('BASE_MODEL_FOLDER')
+    file_list = get_files(folder_name)
+    file_list.sort()
+    return file_list
 
 
-def get_precision_score(to_compare):
-    return precision_score(to_compare, to_compare, average='micro')
+def get_recall_score(base, to_compare):
+    return recall_score(base, to_compare, average='micro')
+
+
+def get_f1_score(base, to_compare):
+    return f1_score(base, to_compare, average='micro', zero_division=1)
+
+
+def get_precision_score(base, to_compare):
+    return precision_score(base, to_compare, average='micro')
+
+
+def get_values_from_dict(data):
+    result = []
+    for key, value in data.items():
+        result.append(value)
+    return result
 
 
 def evaluation(method, file):
-    folder_name: str = '/Users/alemiranda/Desktop/tfg/uploaded_methods/' + str(method['name']) + str(round(time.time()))
+    folder_name: str = os.getenv('UPLOADED_METHODS_FOLDER') + str(method['name']) + str(round(time.time()))
     extract_zip(folder_name, file)
     file_list = get_files(folder_name)
+    file_list.sort()
+    base_model = get_base_model()
+
     f_score = []
     r_score = []
     p_score = []
-    for file in file_list:
+
+    for file, base in zip(file_list, base_model):
         data = json_to_dict(file)
-        result = []
-        for key, value in data.items():
-            result.append(value)
-        f_score.append(get_f1_score(result))
-        r_score.append((get_recall_score(result)))
-        p_score.append((get_precision_score(result)))
+        base_data = json_to_dict(base)
+
+        result = get_values_from_dict(data)
+        base_result = get_values_from_dict(base_data)
+
+        f_score.append(get_f1_score(base_result, result))
+        r_score.append((get_recall_score(base_result, result)))
+        p_score.append((get_precision_score(base_result, result)))
+
+        break
+
     method['results'].append({'f1_score': np.mean(f_score)})
     method['results'].append({'recall_score': np.mean(r_score)})
     method['results'].append({'precision_score': np.mean(p_score)})
