fff4ecfaa8ef2e64ac83b2a52067c93e3956a532
Alejandro Miranda <alejandrom@secret-source.eu>
2022-01-27 21:55:57 +0000

    Actualizado al nuevo modelo

--- app/server/views/Method_view.py
+++ app/server/views/Method_view.py
@@ -2,14 +2,14 @@ import io
 import json
 from typing import Optional, List
 
-from fastapi import APIRouter, Response, status, Body, Depends, File
+from fastapi import APIRouter, Body, Depends, File, HTTPException
 from fastapi.encoders import jsonable_encoder
 from starlette.responses import StreamingResponse
 
 from app.server.auth.auth_bearer import JWTBearer
 from app.server.controllers.Method_controller import find_all, find_by_id, create_method, find_by_user_id, \
     update_method, delete_method, download_all_methods, update_and_evaluate
-from app.server.models.CustomResponse import error_response
+from app.server.models.CustomResponse import ErrorResponse
 from app.server.models.Method import MethodSchema
 
 router = APIRouter(
@@ -18,24 +18,31 @@ router = APIRouter(
 )
 
 
-@router.get("/all", response_model=List[MethodSchema])
-async def get_all_methods(response: Response):
+@router.get("/all",
+            responses={
+                200: {"model": MethodSchema},
+                500: {"model": ErrorResponse}
+            })
+async def get_all_methods():
     methods = find_all()
     if len(methods) <= 0:
-        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-        return error_response("No hemos encontrado ningún resultado")
+        raise HTTPException(500, "No hemos encontrado ningún resultado")
     return methods
 
 
-@router.get("/user_methods", response_model=List[MethodSchema], dependencies=[Depends(JWTBearer())])
-async def get_method_by_user_id(response: Response, user_id: str = None):
+@router.get("/user_methods",
+            responses={
+                200: {"model": List[MethodSchema]},
+                400: {"model": ErrorResponse},
+                500: {"model": ErrorResponse}
+            },
+            dependencies=[Depends(JWTBearer())])
+async def get_method_by_user_id(user_id: str = None):
     if user_id is None:
-        response.status_code = status.HTTP_400_BAD_REQUEST
-        return error_response("Error al buscar los metodos del usuario")
+        raise HTTPException(400, "Error al buscar los metodos del usuario")
     methods = find_by_user_id(user_id)
     if not methods:
-        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-        return error_response("No se ha encontrado ningún resultado para este usuario")
+        raise HTTPException(500, "No se ha encontrado ningún resultado para este usuario")
     return methods
 
 
@@ -61,34 +68,46 @@ async def download_json():
     return file
 
 
-@router.get("/{method_id}", response_model=MethodSchema)
-async def get_method_by_id(method_id: str, response: Response):
+@router.get("/{method_id}",
+            responses={
+                200: {"model": MethodSchema},
+                400: {"model": ErrorResponse},
+                404: {"model": ErrorResponse}
+            })
+async def get_method_by_id(method_id: str):
     if len(method_id) < 24:
-        response.status_code = status.HTTP_400_BAD_REQUEST
-        return error_response("No ha sido posible completar la operación")
+        raise HTTPException(400, "No ha sido posible completar la operación")
     method = find_by_id(method_id)
     if not method:
-        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-        return error_response("No hemos encontrado ningún resultado")
+        raise HTTPException(404, "No se ha podido encontrar este método")
     else:
         return method
 
 
-@router.post("/", response_model=MethodSchema, dependencies=[Depends(JWTBearer())])
-async def upload_method(response: Response, file: bytes = File(...), data: str = Body(...)):
+@router.post("/",
+             status_code=201,
+             responses={
+                 201: {"model": MethodSchema},
+                 500: {"model": ErrorResponse}
+             },
+             dependencies=[Depends(JWTBearer())])
+async def upload_method(file: bytes = File(...), data: str = Body(...)):
     data_json = json.loads(data)
     data = jsonable_encoder(data_json)
     file = io.BytesIO(file)
     new_method = create_method(data, file)
     if not new_method:
-        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-        return error_response("No se ha podido subir el método")
-    response.status_code = status.HTTP_201_CREATED
+        raise HTTPException(500, "No se ha podido completar la solicitud")
     return new_method
 
 
-@router.put("/{method_id}", response_model=MethodSchema, dependencies=[Depends(JWTBearer())])
-async def modify_method(response: Response, method_id: str, file: Optional[bytes] = File(None), data: str = Body(...)):
+@router.put("/{method_id}",
+            responses={
+                200: {"model": MethodSchema},
+                422: {"model": ErrorResponse}
+            },
+            dependencies=[Depends(JWTBearer())])
+async def modify_method(method_id: str, file: Optional[bytes] = File(None), data: str = Body(...)):
     data_json = json.loads(data)
     data = jsonable_encoder(data_json)
     if file is not None:
@@ -97,15 +116,17 @@ async def modify_method(response: Response, method_id: str, file: Optional[bytes
     else:
         updated = update_method(method_id, data)
     if not updated:
-        response.status_code = status.HTTP_422_UNPROCESSABLE_ENTITY
-        return error_response("No ha sido posible completar la operación")
+        raise HTTPException(422, "No se ha podido completar la acción")
     return updated
 
 
-@router.delete("/{method_id}", dependencies=[Depends(JWTBearer())])
-async def remove_method(response: Response, method_id: str):
+@router.delete("/{method_id}",
+               responses={
+                   404: {"model": ErrorResponse}
+               },
+               dependencies=[Depends(JWTBearer())])
+async def remove_method(method_id: str):
     removed = delete_method(method_id)
     if not removed:
-        response.status_code = status.HTTP_404_NOT_FOUND
-        return error_response("No ha sido posible completar la operación")
+        raise HTTPException(404, "No ha sido posible completar la actualización")
     return {"success": True}
