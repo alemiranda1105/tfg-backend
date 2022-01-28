edb9df297a1004dc51f30a7f3d427ebf2f5617b1
Alejandro Miranda <alejandrom@secret-source.eu>
2022-01-28 10:24:07 +0000

    Eliminado modelo sin uso

--- app/server/models/Method.py
+++ app/server/models/Method.py
@@ -3,10 +3,6 @@ from pydantic import BaseModel, Field
 
 
 class MethodSchema(BaseModel):
-    class Result(BaseModel):
-        name: str
-        result: float
-
     id: str
     user_id: str
     name: str
@@ -30,25 +26,5 @@ class MethodSchema(BaseModel):
                         "m2": 0.5421
                     }
                 ]
-            }
-        }
-
-
-class UploadMethodSchema(BaseModel):
-    user_id: str = Field(...)
-    name: str = Field(...)
-    info: str = Field(...)
-    link: str = Field(...)
-    results: List = Field(...)
-
-    class Config:
-        schema_extra = {
-            "example": {
-                "id": "dasdW231d",
-                "user_id": "1",
-                "name": "test",
-                "info": "This is an example",
-                "link": "www.example.com",
-                "results": []
             }
         }
