d63358b215d66a17bfaa4fb1c52aa7b6afca8928
Alejandro Miranda <amiranda110500@gmail.com>
2022-02-05 11:42:54 +0000

    CORS Configurado

--- app/server/app.py
+++ app/server/app.py
@@ -1,10 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.server.views import Method_view, User_view
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:3000"
+]
 
 app = FastAPI()
 app.include_router(Method_view.router)
 app.include_router(User_view.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 
 @app.get("/")
