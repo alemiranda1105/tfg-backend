6666617b64d2f5007ee3c617ae085ba66edec152
Alejandro Miranda <amiranda110500@gmail.com>
2022-02-28 17:16:16 +0000

    Removed old schema

--- app/server/models/Method.py
+++ app/server/models/Method.py
@@ -28,31 +28,3 @@ class MethodSchema(BaseModel):
                 }
             }
         }
-
-
-class OldMethodSchema(BaseModel):
-    id: str
-    user_id: str
-    name: str
-    info: str
-    link: str
-    results: List
-
-    class Config:
-        schema_extra = {
-            "example": {
-                "id": "dasdW231d",
-                "user_id": "1",
-                "name": "test",
-                "info": "This is an example",
-                "link": "www.example.com",
-                "results": [
-                    {
-                        "m1": 0.9192
-                    },
-                    {
-                        "m2": 0.5421
-                    }
-                ]
-            }
-        }
