bbdf4feef84c76db3479e20b3e3faab9019eedd5
Alejandro Miranda <amiranda110500@gmail.com>
2022-03-06 10:18:53 +0000

    HU-14 added privacy field to method model

--- app/server/models/Method.py
+++ app/server/models/Method.py
@@ -6,6 +6,7 @@ class NewMethodModel(BaseModel):
     user_id: str = Field(...)
     info: str = Field(max_length=200, min_length=5)
     link: str = Field(max_length=50, min_length=3)
+    privacy: bool = Field(...)
 
 
 class MethodSchema(BaseModel):
@@ -14,6 +15,7 @@ class MethodSchema(BaseModel):
     name: str = Field(max_length=25, min_length=3)
     info: str = Field(max_length=200, min_length=5)
     link: str = Field(max_length=50, min_length=3)
+    privacy: bool = Field(...)
     results: dict = Field(...)
 
     class Config:
