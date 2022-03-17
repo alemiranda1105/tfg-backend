1d9b460ae8613c6d543ea4096f982c3ca4e10760
Alejandro Miranda <amiranda110500@gmail.com>
2022-03-17 18:30:30 +0000

    fixed str port error?

--- app/main.py
+++ app/main.py
@@ -2,4 +2,4 @@ import uvicorn
 from os import environ
 
 if __name__ == "__main__":
-    uvicorn.run("server.app:app", host="0.0.0.0", port=environ.get("PORT", 8000), reload=True)
+    uvicorn.run("server.app:app", host="0.0.0.0", port=int(environ.get("PORT", 8000)), reload=True)
