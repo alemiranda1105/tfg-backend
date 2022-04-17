62e61ba81fe961f83c648a3f99c77c0459a154b2
Alejandro Miranda <amiranda110500@gmail.com>
2022-04-17 13:33:52 +0100

    HU-23 added content tests

--- app/tests/mocked_test_data.py
+++ app/tests/mocked_test_data.py
@@ -1,37 +1,38 @@
 from app.server.auth.auth_handler import sign_jwt
 
-mocked_jwt = sign_jwt("1")['token']
+mocked_jwt = sign_jwt("1", "user")['token']
+mocked_jwt_admin = sign_jwt("1", "admin")['token']
 
 methods_data_test = [
-        {
-            "user_id": "1",
-            "name": "test",
-            "info": "This is an example",
-            "link": "https://www.example.com",
-            "private": True,
-            "anonymous": False,
-            "results": {}
-        },
-        {
-            "user_id": "1",
-            "name": "test2",
-            "info": "This is an example2",
-            "link": "https://www.example.com",
-            "source_code": "https://www.example.com",
-            "private": False,
-            "anonymous": False,
-            "results": {}
-        },
-        {
-            "user_id": "1",
-            "name": "test3",
-            "info": "This is an example3",
-            "link": "https://www.example.com",
-            "private": False,
-            "anonymous": False,
-            "results": {}
-        }
-    ]
+    {
+        "user_id": "1",
+        "name": "test",
+        "info": "This is an example",
+        "link": "https://www.example.com",
+        "private": True,
+        "anonymous": False,
+        "results": {}
+    },
+    {
+        "user_id": "1",
+        "name": "test2",
+        "info": "This is an example2",
+        "link": "https://www.example.com",
+        "source_code": "https://www.example.com",
+        "private": False,
+        "anonymous": False,
+        "results": {}
+    },
+    {
+        "user_id": "1",
+        "name": "test3",
+        "info": "This is an example3",
+        "link": "https://www.example.com",
+        "private": False,
+        "anonymous": False,
+        "results": {}
+    }
+]
 
 user_data_test = [
     {
@@ -48,5 +49,32 @@ user_data_test = [
         "username": "test3",
         "email": "test3@test.com",
         "password": "123456"
+    }
+]
+
+content_data_test = [
+    {
+        "title": "About",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at fermentum magna, ac ultrices eros. "
+                "Ut lacinia nisl eu urna ornare scelerisque. Aenean scelerisque mi vitae ipsum molestie, at lobortis "
+                "purus iaculis. Nam ultrices rutrum metus, vulputate commodo lorem posuere at. Pellentesque et ligula "
+                "egestas, rhoncus mi at, tempus sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+                "Nullam lorem diam, eleifend a mauris id, sodales volutpat velit. Etiam sit amet dapibus risus, "
+                "id tincidunt enim. Aliquam sit amet enim maximus, tincidunt mauris in, congue turpis. Donec arcu "
+                "augue, feugiat sit amet nulla non, lobortis auctor sem. Integer non justo dignissim, tristique nulla "
+                "at, lacinia lectus. Curabitur dictum convallis justo sed elementum. Vivamus nec pellentesque dolor, "
+                "et convallis leo. "
+    },
+    {
+        "title": "Description",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at fermentum magna, ac ultrices eros. "
+                "Ut lacinia nisl eu urna ornare scelerisque. Aenean scelerisque mi vitae ipsum molestie, at lobortis "
+                "purus iaculis. Nam ultrices rutrum metus, vulputate commodo lorem posuere at. Pellentesque et ligula "
+                "egestas, rhoncus mi at, tempus sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+                "Nullam lorem diam, eleifend a mauris id, sodales volutpat velit. Etiam sit amet dapibus risus, "
+                "id tincidunt enim. Aliquam sit amet enim maximus, tincidunt mauris in, congue turpis. Donec arcu "
+                "augue, feugiat sit amet nulla non, lobortis auctor sem. Integer non justo dignissim, tristique nulla "
+                "at, lacinia lectus. Curabitur dictum convallis justo sed elementum. Vivamus nec pellentesque dolor, "
+                "et convallis leo. "
     }
 ]
---
+++ app/tests/test_content.py
@@ -0,0 +1,96 @@
+from starlette.testclient import TestClient
+from app.server.app import app
+from app.tests.mocked_test_data import content_data_test, mocked_jwt, mocked_jwt_admin
+
+client = TestClient(app)
+
+inserted_content = []
+
+
+def test_get_all_error():
+    response = client.get("methods/all")
+    assert response.status_code == 404
+
+
+def test_create_content_error():
+    content = content_data_test[0]
+    response = client.post(
+        "content/",
+        headers={
+            'Authorization': 'Bearer {}'.format(mocked_jwt)
+        },
+        json=content
+    )
+    assert response.status_code == 403
+
+
+def test_create_content():
+    for c in content_data_test:
+        response = client.post(
+            "content/",
+            headers={
+                'Authorization': 'Bearer {}'.format(mocked_jwt_admin)
+            },
+            json=c
+        )
+        assert response.status_code == 200
+        data = response.json()
+        inserted_content.append(data['id'])
+    assert len(inserted_content) == len(content_data_test)
+
+
+def test_get_all():
+    response = client.get("content/")
+    assert response.status_code == 200
+    data = response.json()
+    assert len(data) == len(content_data_test)
+
+
+def test_get_by_title():
+    response = client.get("content/{}".format(content_data_test[0]['title']))
+    assert response.status_code == 200
+    data = response.json()
+    assert data['title'] == content_data_test[0]['title']
+
+
+def test_get_by_title_error():
+    response = client.get("content/error")
+    assert response.status_code == 404
+
+
+def test_update_content():
+    updated = content_data_test[0]
+    updated['text'] = "text updated"
+    response = client.put(
+        "content/{}".format(inserted_content[0]),
+        headers={
+            'Authorization': 'Bearer {}'.format(mocked_jwt_admin)
+        },
+        json=updated
+    )
+    assert response.status_code == 200
+    data = response.json()
+    assert data['text'] != content_data_test[0]
+
+
+def test_delete_content_error():
+    for c in inserted_content:
+        response = client.delete(
+            "content/{}".format(c),
+            headers={
+                'Authorization': 'Bearer {}'.format(mocked_jwt)
+            })
+        assert response.status_code == 403
+
+
+def test_delete_content():
+    deleted = 0
+    for c in inserted_content:
+        response = client.delete(
+            "content/{}".format(c),
+            headers={
+                'Authorization': 'Bearer {}'.format(mocked_jwt_admin)
+            })
+        assert response.status_code == 200
+        deleted += 1
+    assert deleted == len(inserted_content)
