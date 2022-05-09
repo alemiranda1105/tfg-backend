319c17ca3f225c8213fa9e7ff5c385b9964c9fa8
Alejandro Miranda <amiranda110500@gmail.com>
2022-05-09 16:50:37 +0100

    test updated

--- app/server/controllers/User_controller.py
+++ app/server/controllers/User_controller.py
@@ -69,7 +69,7 @@ async def update_password(user_id: str, passwords) -> bool:
 
 # Only updates username and/or email
 def update_user(user_id: str, user_data):
-    if user_validation_helper(user_data, user_id, "signup"):
+    if user_validation_helper(user_data, user_id, "sign_up"):
         user_data['password'] = hash_password(user_data['password'])
         user_id = ObjectId(user_id)
         old = users_collection.find_one({"_id": user_id})
@@ -97,7 +97,8 @@ def update_user(user_id: str, user_data):
 
             updated = users_collection.update_one({"_id": user_id}, {"$set": user_data})
             if updated:
-                if user_validation_helper(updated, str(user_id), "registered"):
+                user = users_collection.find_one({"_id": user_id})
+                if user_validation_helper(user, str(user_id), "registered"):
                     new_user_data = users_collection.find_one({"_id": user_id})
                     return user_profile_helper(new_user_data)
     return False
--- tests/test_content.py
+++ tests/test_content.py
@@ -1,6 +1,6 @@
 from starlette.testclient import TestClient
 from app.server.app import app
-from app.tests.mocked_test_data import content_data_test, mocked_jwt, mocked_jwt_admin
+from .mocked_test_data import content_data_test, mocked_jwt, mocked_jwt_admin
 
 client = TestClient(app)
 
@@ -15,7 +15,7 @@ def test_get_all_error():
 def test_create_content_error():
     content = content_data_test[0]
     response = client.post(
-        "content/",
+        "idsemapi/content/",
         headers={
             'Authorization': 'Bearer {}'.format(mocked_jwt)
         },
@@ -27,7 +27,7 @@ def test_create_content_error():
 def test_create_content():
     for c in content_data_test:
         response = client.post(
-            "content/",
+            "idsemapi/content/",
             headers={
                 'Authorization': 'Bearer {}'.format(mocked_jwt_admin)
             },
@@ -40,21 +40,21 @@ def test_create_content():
 
 
 def test_get_all():
-    response = client.get("content/")
+    response = client.get("idsemapi/content/")
     assert response.status_code == 200
     data = response.json()
     assert len(data) == len(content_data_test)
 
 
 def test_get_by_title():
-    response = client.get("content/{}".format(content_data_test[0]['title']))
+    response = client.get("idsemapi/content/{}".format(content_data_test[0]['title']))
     assert response.status_code == 200
     data = response.json()
     assert data['title'] == content_data_test[0]['title']
 
 
 def test_get_by_title_error():
-    response = client.get("content/error")
+    response = client.get("idsemapi/content/error")
     assert response.status_code == 404
 
 
@@ -62,7 +62,7 @@ def test_update_content():
     updated = content_data_test[0]
     updated['text'] = "text updated"
     response = client.put(
-        "content/{}".format(inserted_content[0]),
+        "idsemapi/content/{}".format(inserted_content[0]),
         headers={
             'Authorization': 'Bearer {}'.format(mocked_jwt_admin)
         },
@@ -76,7 +76,7 @@ def test_update_content():
 def test_delete_content_error():
     for c in inserted_content:
         response = client.delete(
-            "content/{}".format(c),
+            "idsemapi/content/{}".format(c),
             headers={
                 'Authorization': 'Bearer {}'.format(mocked_jwt)
             })
@@ -87,7 +87,7 @@ def test_delete_content():
     deleted = 0
     for c in inserted_content:
         response = client.delete(
-            "content/{}".format(c),
+            "idsemapi/content/{}".format(c),
             headers={
                 'Authorization': 'Bearer {}'.format(mocked_jwt_admin)
             })
